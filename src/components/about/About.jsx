e8720740b5868d6a9fd141c5b4c6db7cb778d8c7
GURU PANDI M <gurupandiguru1@gmail.com>
2026-01-07 21:26:20 +0530

    Fix import path for Guru.pdf in About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./About.css";
 import guru from "./guru.jpg";
 import Resume from "../resume/Resume";
-import pdf from "./Guru.pdf"
+import pdf from "./guru.pdf"
 import boot from "./skills/bootstrap.png";
 import css from "./skills/css.png";
 import express from "./skills/express js.png";
